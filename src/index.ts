22e6fac2cf96da2983fdee60ec9479eb7d57c6da
Dimitar-Stoimenov <dstoimen0@gmail.com>
2023-08-08 13:07:55 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -2,128 +2,39 @@ import { getInput, setFailed } from '@actions/core';
 import * as fs from 'fs';
 
 async function run() {
-	let deltaInput = Number(getInput("delta"));
-	deltaInput = isNaN(deltaInput) || deltaInput === undefined ? 0 : -deltaInput;
+	// let deltaInput = Number(getInput("delta"));
+	// deltaInput = isNaN(deltaInput) || deltaInput === undefined ? 0 : -deltaInput;
 
-    const baseClientPath = './coverage-base/short/shortClient.txt';
-    const baseServerPath = './coverage-base/short/shortServer.txt';
-    const prClientPath = './coverage-PR/short/shortClient.txt';
-    const prServerPath = './coverage-PR/short/shortServer.txt';
-
-    const getParams = (input: string) => {
-        const regex = /^(?<name>[a-zA-Z]+)\s*:\s(?<percentage>[0-9].*)%\s\(\s(?<tested>[0-9]*)\/(?<total>[0-9]*)\s\)/g;
-
-        const match = regex.exec(input);
-
-        if (match?.groups) {
-            return {
-                name: match.groups.name,
-                percentage: +match.groups.percentage,
-                tested: +match.groups.tested,
-                total: +match.groups.total
-            }
-        }
-
-        return null;
-    }
-
-    type FinalData = {
-        [x: string]: number
-    }
+    const basePath = './coverage-diff/base-summary.json';
+    const prPath = './coverage-diff/pr-summary.json';
 
     try {
-        const BCdata = fs.readFileSync(baseClientPath, 'utf8');
-        const BCarr = BCdata.split(/\r?\n/);
-        const BCFinalData: FinalData = {}; 
+		const baseResultJSON = fs.readFileSync(basePath, 'utf8');
+		const prResultJSON = fs.readFileSync(prPath, 'utf8');
 
-        const BSdata = fs.readFileSync(baseServerPath, 'utf8');
-        const BSarr = BSdata.split(/\r?\n/);
-        const BSFinalData: FinalData = {}; 
+		const baseResultObject = JSON.parse(baseResultJSON);
+		const prResultObject = JSON.parse(prResultJSON);
 
-        const PCdata = fs.readFileSync(prClientPath, 'utf8');
-        const PCarr = PCdata.split(/\r?\n/);
-        const PCFinalData: FinalData = {}; 
+		console.log('base total:')
+		const keys = Object.keys(baseResultObject);
 
-        const PSdata = fs.readFileSync(prServerPath, 'utf8');
-        const PSarr = PSdata.split(/\r?\n/);
-        const PSFinalData: FinalData = {}; 
-
-		for (let i = 1; i < BCarr.length; i++) {
-			const element = BCarr[i];
-
-			const result = getParams(element);
-
-			if (!result) continue;
-			BCFinalData[result.name] = result.percentage;        
-		}
-
-		for (let i = 1; i < BSarr.length; i++) {
-			const element = BSarr[i];
-
-			const result = getParams(element);
-
-			if (!result) continue;
-			BSFinalData[result.name] = result.percentage;        
-		}
-
-		for (let i = 1; i < PCarr.length; i++) {
-			const element = PCarr[i];
-
-			const result = getParams(element);
-
-			if (!result) continue;
-			PCFinalData[result.name] = result.percentage;        
-		}
-
-		for (let i = 1; i < PSarr.length; i++) {
-			const element = PSarr[i];
-
-			const result = getParams(element);
-
-			if (!result) continue;
-			PSFinalData[result.name] = result.percentage;        
-		}
-
-		const serverStatementsDiff = PSFinalData.Statements - BSFinalData.Statements;
-		const serverBranchesDiff = PSFinalData.Branches - BSFinalData.Branches;
-		const serverFunctionsDiff = PSFinalData.Functions - BSFinalData.Functions;
-		const serverLinesDiff = PSFinalData.Lines - BSFinalData.Lines;
-
-		const clientStatementsDiff = PCFinalData.Statements - BCFinalData.Statements;
-		const clientBranchesDiff = PCFinalData.Branches - BCFinalData.Branches;
-		const clientFunctionsDiff = PCFinalData.Functions - BCFinalData.Functions;
-		const clientLinesDiff = PCFinalData.Lines - BCFinalData.Lines;
-
-		console.log("================================ Server summary ================================")
-		console.log(`Statements   : ${serverStatementsDiff > 0 ? "+" + serverStatementsDiff.toFixed(2) : serverStatementsDiff.toFixed(2)}%`)
-		console.log(`Branches     : ${serverBranchesDiff > 0 ? "+" + serverBranchesDiff.toFixed(2) : serverBranchesDiff.toFixed(2)}%`)
-		console.log(`Functions    : ${serverFunctionsDiff > 0 ? "+" + serverFunctionsDiff.toFixed(2) : serverFunctionsDiff.toFixed(2)}%`)
-		console.log(`Lines        : ${serverLinesDiff > 0 ? "+" + serverLinesDiff.toFixed(2) : serverLinesDiff.toFixed(2)}%`)
-		console.log("================================================================================");
-		console.log(" ")
-		console.log("================================ Client summary ================================")
-		console.log(`Statements   : ${clientStatementsDiff > 0 ? "+" + clientStatementsDiff.toFixed(2) : clientStatementsDiff.toFixed(2)}%`)
-		console.log(`Branches     : ${clientBranchesDiff > 0 ? "+" + clientBranchesDiff.toFixed(2) : clientBranchesDiff.toFixed(2)}%`)
-		console.log(`Functions    : ${clientFunctionsDiff > 0 ? "+" + clientFunctionsDiff.toFixed(2) : clientFunctionsDiff.toFixed(2)}%`)
-		console.log(`Lines        : ${clientLinesDiff > 0 ? "+" + clientLinesDiff.toFixed(2) : clientLinesDiff.toFixed(2)}%`)
-		console.log("================================================================================");
-
-      	if (
-        	serverStatementsDiff < deltaInput
-			|| serverBranchesDiff < deltaInput
-			|| serverFunctionsDiff < deltaInput
-			|| serverLinesDiff < deltaInput
-			|| clientStatementsDiff < deltaInput
-			|| clientBranchesDiff < deltaInput
-			|| clientFunctionsDiff < deltaInput
-			|| clientLinesDiff < deltaInput
-        ) {
-			const error = new Error();
-			error.message = "The coverage is worse than before! You need to write some tests!"
-			throw error;
-		}
-
-		console.log("The coverage is OK.")
+		console.log(keys[15])
+		console.log(keys[125])
+		console.log(keys[235])
+		console.log(keys[245])
+		// console.log("================================ Server summary ================================")
+		// console.log(`Statements   : ${serverStatementsDiff > 0 ? "+" + serverStatementsDiff.toFixed(2) : serverStatementsDiff.toFixed(2)}%`)
+		// console.log(`Branches     : ${serverBranchesDiff > 0 ? "+" + serverBranchesDiff.toFixed(2) : serverBranchesDiff.toFixed(2)}%`)
+		// console.log(`Functions    : ${serverFunctionsDiff > 0 ? "+" + serverFunctionsDiff.toFixed(2) : serverFunctionsDiff.toFixed(2)}%`)
+		// console.log(`Lines        : ${serverLinesDiff > 0 ? "+" + serverLinesDiff.toFixed(2) : serverLinesDiff.toFixed(2)}%`)
+		// console.log("================================================================================");
+		// console.log(" ")
+		// console.log("================================ Client summary ================================")
+		// console.log(`Statements   : ${clientStatementsDiff > 0 ? "+" + clientStatementsDiff.toFixed(2) : clientStatementsDiff.toFixed(2)}%`)
+		// console.log(`Branches     : ${clientBranchesDiff > 0 ? "+" + clientBranchesDiff.toFixed(2) : clientBranchesDiff.toFixed(2)}%`)
+		// console.log(`Functions    : ${clientFunctionsDiff > 0 ? "+" + clientFunctionsDiff.toFixed(2) : clientFunctionsDiff.toFixed(2)}%`)
+		// console.log(`Lines        : ${clientLinesDiff > 0 ? "+" + clientLinesDiff.toFixed(2) : clientLinesDiff.toFixed(2)}%`)
+		// console.log("================================================================================");
     } catch (error) {
         setFailed((error as Error)?.message ?? 'Unknown error');
     }
