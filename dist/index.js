c40557cc8442d48b94f6fb8b3fcf56917f69e93f
Dimitar-Stoimenov <dstoimen0@gmail.com>
2023-08-08 17:28:50 +0300

    should work now

--- dist/index.js
+++ dist/index.js
@@ -32,15 +32,43 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __nccwpck_require__(186);
 const fs = __importStar(__nccwpck_require__(147));
-const compareFileCoverage = (prFile, baseFile, fileName) => {
-    if (!baseFile) {
-        console.log(`${fileName} is a new file. Write tests for it!`);
-    }
-};
 async function run() {
     var _a;
+    let diffs = [];
     const basePath = './coverage-base/coverage-summary.json';
     const prPath = './coverage-pr/coverage-summary.json';
+    const compareFileCoverage = (prFileObj, baseFileObj, fileName) => {
+        let diffCheck = false;
+        let result = {
+            linesPct: 0,
+            functionsPct: 0,
+            statementsPct: 0,
+            branchesPct: 0
+        };
+        if (!baseFileObj) {
+            diffs.push(`${fileName} is a new or renamed file. Write tests for it!`);
+            return null;
+        }
+        if (prFileObj.lines.pct < baseFileObj.lines.pct) {
+            diffCheck = true;
+            result.linesPct = prFileObj.lines.pct - baseFileObj.lines.pct;
+        }
+        if (prFileObj.functions.pct < baseFileObj.functions.pct) {
+            diffCheck = true;
+            result.functionsPct = prFileObj.functions.pct - baseFileObj.functions.pct;
+        }
+        if (prFileObj.statements.pct < baseFileObj.statements.pct) {
+            diffCheck = true;
+            result.statementsPct = prFileObj.statements.pct - baseFileObj.statements.pct;
+        }
+        if (prFileObj.branches.pct < baseFileObj.branches.pct) {
+            diffCheck = true;
+            result.branchesPct = prFileObj.branches.pct - baseFileObj.branches.pct;
+        }
+        if (!diffCheck)
+            return null;
+        return result;
+    };
     try {
         const baseResultJSON = fs.readFileSync(basePath, 'utf8');
         const prResultJSON = fs.readFileSync(prPath, 'utf8');
@@ -53,19 +81,47 @@ async function run() {
         const functionsDiff = prResultTotal.functions.pct - baseResultTotal.functions.pct;
         const linesDiff = prResultTotal.lines.pct - baseResultTotal.lines.pct;
         const prFiles = Object.keys(prResultObject);
-        for (let i = 0; i < prFiles.length; i++) {
+        for (let i = 1; i < prFiles.length; i++) {
             const fileName = prFiles[i];
-            const result = compareFileCoverage(prResultObject[fileName], baseResultObject[fileName], fileName);
+            const prFileCoverageObj = prResultObject[fileName];
+            const baseFileCoverageObj = baseResultObject[fileName];
+            const result = compareFileCoverage(prFileCoverageObj, baseFileCoverageObj, fileName);
+            if (!result)
+                continue;
+            const statementsMsg = result.statementsPct < 0 ? `Statements Diff: ${result.statementsPct.toFixed(2)}%` : "";
+            const branchesMsg = result.branchesPct < 0 ? `Branches Diff: ${result.branchesPct.toFixed(2)}%` : "";
+            const functionsMsg = result.functionsPct < 0 ? `Functions Diff: ${result.functionsPct.toFixed(2)}%` : "";
+            const linesMsg = result.linesPct < 0 ? `Lines Diff: ${result.linesPct.toFixed(2)}%` : "";
+            const message = [statementsMsg, branchesMsg, functionsMsg, linesMsg].join(" ").trim();
+            diffs.push(`${fileName} >>> ${message}`);
         }
-        console.log("=============================== Coverage summary ===============================");
+        console.log("============================== Coverage difference =============================");
         console.log(`Statements   : ${statementsDiff > 0 ? "+" + statementsDiff.toFixed(2) : statementsDiff.toFixed(2)}%`);
         console.log(`Branches     : ${branchesDiff > 0 ? "+" + branchesDiff.toFixed(2) : branchesDiff.toFixed(2)}%`);
         console.log(`Functions    : ${functionsDiff > 0 ? "+" + functionsDiff.toFixed(2) : functionsDiff.toFixed(2)}%`);
         console.log(`Lines        : ${linesDiff > 0 ? "+" + linesDiff.toFixed(2) : linesDiff.toFixed(2)}%`);
         console.log("================================================================================");
+        console.log("");
+        for (let i = 0; i < diffs.length; i++) {
+            if (i === 0) {
+                console.log("=========================== Files with worse coverage ==========================");
+            }
+            const diff = diffs[i];
+            console.log(diff);
+            if (i === diffs.length - 1) {
+                console.log("================================================================================");
+                console.log("");
+            }
+        }
+        if (diffs.length > 0) {
+            throw new Error("Coverage action failed - Write more tests!");
+        }
+        else {
+            console.log("Coverage is OK.");
+        }
     }
     catch (error) {
-        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : 'Unknown error');
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : 'Unknown error.');
     }
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getInput, setFailed } from '@actions/core';
+import { setFailed } from '@actions/core';
 import * as fs from 'fs';
 
 interface CoverageInterface {
@@ -19,16 +19,58 @@ interface JSONInterface {
 	[x: string]: SingleFileInterface;
 }
 
-const compareFileCoverage = (prFile: SingleFileInterface, baseFile: SingleFileInterface, fileName: string) => {
-	if (!baseFile) {
-		console.log(`${fileName} is a new file. Write tests for it!`)
-	}
-
-};
+type CompareResult = {
+	linesPct: number;
+	functionsPct: number;
+	statementsPct: number;
+	branchesPct: number;
+} | null;
 
 async function run() {
+	let diffs: string[] = [];
+
     const basePath = './coverage-base/coverage-summary.json';
     const prPath = './coverage-pr/coverage-summary.json';
+
+	const compareFileCoverage = (prFileObj: SingleFileInterface, baseFileObj: SingleFileInterface, fileName: string): CompareResult => {
+		let diffCheck = false;
+		let result = {
+			linesPct: 0,
+			functionsPct: 0,
+			statementsPct: 0,
+			branchesPct: 0
+		};
+
+		if (!baseFileObj) {
+			diffs.push(`${fileName} is a new or renamed file. Write tests for it!`);
+
+			return null;
+		}
+
+		if (prFileObj.lines.pct < baseFileObj.lines.pct) {
+			diffCheck = true;
+			result.linesPct = prFileObj.lines.pct - baseFileObj.lines.pct;
+		}
+
+		if (prFileObj.functions.pct < baseFileObj.functions.pct) {
+			diffCheck = true;
+			result.functionsPct = prFileObj.functions.pct - baseFileObj.functions.pct;
+		}
+
+		if (prFileObj.statements.pct < baseFileObj.statements.pct) {
+			diffCheck = true;
+			result.statementsPct = prFileObj.statements.pct - baseFileObj.statements.pct;
+		}
+
+		if (prFileObj.branches.pct < baseFileObj.branches.pct) {
+			diffCheck = true;
+			result.branchesPct = prFileObj.branches.pct - baseFileObj.branches.pct;
+		}
+
+		if (!diffCheck) return null;
+	
+		return result;
+	};
 
     try {
 		const baseResultJSON = fs.readFileSync(basePath, 'utf8');
@@ -47,23 +89,55 @@ async function run() {
 
 		const prFiles = Object.keys(prResultObject);
 
-		for (let i = 0; i < prFiles.length; i++) {
+		for (let i = 1; i < prFiles.length; i++) {
 			const fileName = prFiles[i];
 
-			const result = compareFileCoverage(prResultObject[fileName], baseResultObject[fileName], fileName);
-			
+			const prFileCoverageObj = prResultObject[fileName];
+			const baseFileCoverageObj = baseResultObject[fileName];
 
+			const result = compareFileCoverage(prFileCoverageObj, baseFileCoverageObj, fileName);			
+			if (!result) continue;
+
+			const statementsMsg = result.statementsPct < 0 ? `Statements Diff: ${result.statementsPct.toFixed(2)}%` : ""
+			const branchesMsg = result.branchesPct < 0 ? `Branches Diff: ${result.branchesPct.toFixed(2)}%` : ""
+			const functionsMsg = result.functionsPct < 0 ? `Functions Diff: ${result.functionsPct.toFixed(2)}%` : ""
+			const linesMsg = result.linesPct < 0 ? `Lines Diff: ${result.linesPct.toFixed(2)}%` : ""
+			const message = [statementsMsg, branchesMsg, functionsMsg, linesMsg].join(" ").trim();
+
+			diffs.push(`${fileName} >>> ${message}`);
 		}
 
-		console.log("=============================== Coverage summary ===============================")
+		console.log("============================== Coverage difference =============================")
 		console.log(`Statements   : ${statementsDiff > 0 ? "+" + statementsDiff.toFixed(2) : statementsDiff.toFixed(2)}%`)
 		console.log(`Branches     : ${branchesDiff > 0 ? "+" + branchesDiff.toFixed(2) : branchesDiff.toFixed(2)}%`)
 		console.log(`Functions    : ${functionsDiff > 0 ? "+" + functionsDiff.toFixed(2) : functionsDiff.toFixed(2)}%`)
 		console.log(`Lines        : ${linesDiff > 0 ? "+" + linesDiff.toFixed(2) : linesDiff.toFixed(2)}%`)
 		console.log("================================================================================");
+		console.log("")
+
+		for (let i = 0; i < diffs.length; i++) {
+			if (i === 0) {
+				console.log("=========================== Files with worse coverage ==========================")
+			}
+
+			const diff = diffs[i];
+			console.log(diff);
+
+			if (i === diffs.length - 1) {
+				console.log("================================================================================")
+				console.log("")
+			}
+		}
+
+		if (diffs.length > 0) {
+			throw new Error("Coverage action failed - Write more tests!");
+		} else {
+			console.log("Coverage is OK.")
+		}
+
     } catch (error) {
-        setFailed((error as Error)?.message ?? 'Unknown error');
-    }
+        setFailed((error as Error)?.message ?? 'Unknown error.');    
+	}
 }
 
 run();
